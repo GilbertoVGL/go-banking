3e64bda429cb70a9b7292f480d04d08d2c93d41d
Betim <gvampre1991@gmail.com>
2021-11-14 19:17:07 -0300

    Update tables schema

--- init.sql
+++ init.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS accounts (
 	id serial PRIMARY KEY,
-	created_at date DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	updated_at date DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	name text NOT NULL,
 	cpf text UNIQUE NOT NULL,
 	secret text NOT NULL,
-	balance numeric DEFAULT 0 NOT NULL,
-	active boolean DEFAULT false
+	balance bigint DEFAULT 0 NOT NULL,
+	active boolean DEFAULT true NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS transfers (
 	id serial PRIMARY KEY,
 	account_origin_id bigint REFERENCES accounts(id), 
 	account_destination_id bigint REFERENCES accounts(id),
-	amount numeric,
-	created_at date DEFAULT CURRENT_TIMESTAMP NOT NULL
+	amount bigint,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
