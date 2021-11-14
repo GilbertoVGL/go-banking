230e7375a445120d78d487d38e005ba8b53815cb
Betim <gvampre1991@gmail.com>
2021-11-14 19:21:31 -0300

    Remove unused models

--- pkg/repository/postgresdb/account.go
+++
@@ -1,9 +0,0 @@
-package postgresdb
-
-type Account struct {
-	Name    string
-	Cpf     string
-	Secret  string
-	Balance float64
-	Active  bool
-}
--- pkg/repository/postgresdb/transfer.go
+++
@@ -1,7 +0,0 @@
-package postgresdb
-
-type Transfer struct {
-	account_origin_id      uint
-	account_destination_id uint
-	amount                 float64
-}
