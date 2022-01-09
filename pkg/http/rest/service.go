af36bd222240472758d6cdaace5fccf02a390810
Betim <gvampre1991@gmail.com>
2022-01-08 22:46:48 -0300

    Fix wrong error check condition

--- pkg/http/rest/service.go
+++ pkg/http/rest/service.go
@@ -78,7 +78,7 @@ func doTransfer(s transfer.Service) func(http.ResponseWriter, *http.Request) {
 		}
 
 		if err := s.DoTransfer(newTransfer); err != nil {
-			if _, ok := err.(*apperrors.ArgumentError); !ok {
+			if _, ok := err.(*apperrors.ArgumentError); ok {
 				respondWithError(w, http.StatusBadRequest, err.Error())
 				return
 			}
