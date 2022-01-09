9e865c9d1717f30ff9a67e7fd26aa66481ca6d22
Betim <gvampre1991@gmail.com>
2022-01-08 21:53:41 -0300

    Add app errors

--- pkg/apperrors/apperrors.go
+++ pkg/apperrors/apperrors.go
@@ -22,3 +22,21 @@ type TransferRequestError struct {
 func (e *TransferRequestError) Error() string {
 	return fmt.Sprintf("%s: %s", e.Err.Error(), e.Context)
 }
+
+type AccountNotFoundError struct {
+	Context string
+	Err     error
+}
+
+func (e *AccountNotFoundError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Err.Error(), e.Context)
+}
+
+type DatabaseError struct {
+	Context string
+	Err     error
+}
+
+func (e *DatabaseError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Err.Error(), e.Context)
+}
