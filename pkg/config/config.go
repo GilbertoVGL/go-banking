b0b82ad75099bde5f91fd160ebcdb509312664ee
Betim <gvampre1991@gmail.com>
2022-01-28 14:06:27 -0300

    Add more granular timeout control

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,7 +3,9 @@ package config
 import (
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/GilbertoVGL/go-banking/pkg/apperrors"
 	"github.com/joho/godotenv"
@@ -19,13 +21,28 @@ var requiredEnvs []string = []string{
 	"DB_NAME",
 	"DB_MAX_IDLE_CONN",
 	"DB_MAX_POOL",
+	"RESQUEST_TIMEOUT_S",
+	"SERVER_WRITE_TIMEOUT_S",
+	"SERVER_READ_TIMEOUT_S",
+	"SERVER_IDLE_TIMEOUT_S",
+	"SERVER_READ_HEADER_TIMEOUT_S",
 }
+
+var ServerWriteTimeout time.Duration
+var ServerReadTimeout time.Duration
+var ServerIdleTimeout time.Duration
+var ServerReadHeaderTimeout time.Duration
+var RequestTimeout time.Duration
 
 func Load(path string) error {
 	realPath := filepath.FromSlash(path)
 	godotenv.Load(realPath)
 
 	if err := verifyEnv(); err != nil {
+		return err
+	}
+
+	if err := fillTimeOutValues(); err != nil {
 		return err
 	}
 
@@ -44,6 +61,52 @@ func verifyEnv() error {
 	if len(missing) > 0 {
 		return apperrors.NewEnvVarError("Missing required variables", strings.Join(missing, ", "))
 	}
+
+	return nil
+}
+
+func fillTimeOutValues() error {
+	var invalid []string
+
+	rt, err := strconv.Atoi(os.Getenv("RESQUEST_TIMEOUT_S"))
+
+	if err != nil {
+		invalid = append(invalid, "RESQUEST_TIMEOUT_S")
+	}
+
+	swt, err := strconv.Atoi(os.Getenv("SERVER_WRITE_TIMEOUT_S"))
+
+	if err != nil {
+		invalid = append(invalid, "SERVER_WRITE_TIMEOUT_S")
+	}
+
+	srt, err := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT_S"))
+
+	if err != nil {
+		invalid = append(invalid, "SERVER_READ_TIMEOUT_S")
+	}
+
+	sit, err := strconv.Atoi(os.Getenv("SERVER_IDLE_TIMEOUT_S"))
+
+	if err != nil {
+		invalid = append(invalid, "SERVER_IDLE_TIMEOUT_S")
+	}
+
+	srht, err := strconv.Atoi(os.Getenv("SERVER_READ_HEADER_TIMEOUT_S"))
+
+	if err != nil {
+		invalid = append(invalid, "SERVER_READ_HEADER_TIMEOUT_S")
+	}
+
+	if len(invalid) > 0 {
+		return apperrors.NewEnvVarError("invalid env value", strings.Join(invalid, ", "))
+	}
+
+	RequestTimeout = (time.Duration(rt) * time.Second)
+	ServerWriteTimeout = (time.Duration(swt) * time.Second)
+	ServerReadTimeout = (time.Duration(srt) * time.Second)
+	ServerIdleTimeout = (time.Duration(sit) * time.Second)
+	ServerReadHeaderTimeout = (time.Duration(srht) * time.Second)
 
 	return nil
 }
